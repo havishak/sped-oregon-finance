af164da9eb9f16d73829880f47b5f7e60f3fce00
Havisha Khurana <88750950+havishak@users.noreply.github.com>
2025-03-17 15:14:24 -0700

    list of packages

---
+++ code/replicate.R
@@ -0,0 +1,5 @@
+# To replicate, you need to install the following packages
+install.packages(c("tidyverse", "gghighlight", "geomtextpath", "ggrepel", 
+                   "patchwork", "RColorBrewer", "ggsankey", "sf", 
+                   "readxl", "plotly", "tigris", "pdftools"),
+                 dependencies = TRUE)
